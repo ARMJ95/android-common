0240a007d215c0dd8bee09c0b4c8cbaf67af8db0
Adam Patacchiola <adam@2fours.com>
2014-03-05 09:44:55 -0700

    back to production settings

--- src/com/twofours/surespot/common/SurespotConstants.java
+++ src/com/twofours/surespot/common/SurespotConstants.java
@@ -17,7 +17,7 @@ public class SurespotConstants {
 	public static final String GOOGLE_APP_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAytYzwMl/RLFQWotcjazzM/xbgqU/UCMEjBJ3QgRG2aT0B0+LCaEkq4bL1FnDvXxUmKOAfGnhn7zbFMfgrfTOOvTuioKgspLE2RLryl9K9MnAxBn2rxBv45gG2E+K97gs7qqrxfrAUTTZPfq8McXVLc3k9WWOf/pfiH25WL7ijig3CNkzJ+BP1u2KQiFltsdln7q+9tMddUWUxd8zP6c0hmCIB9DYLutfqmel0ckjfEPAhTKqsOIpryzcGLZCs2C22Qg0AllQ62ohidEIFdA0SYKp0CjO9E5NO1pM+ZnBlf/cmdEwlhuYyh26rUcZj342fNhWzwiiHIeiiedvXOOS8wIDAQAB";
 
 	// PROD
-	public static final String SERVER_PUBLIC_KEY = SERVER_PUBLIC_KEY_LOCAL;
+	public static final String SERVER_PUBLIC_KEY = SERVER_PUBLIC_KEY_PROD;
 
 	public class IntentFilters {
 		public static final String INVITE_REQUEST = "invite_request_intent";
@@ -57,10 +57,10 @@ public class SurespotConstants {
 	
 
 	// PROD set to 3
-	public final static int MAX_IDENTITIES = 30;
+	public final static int MAX_IDENTITIES = 3;
 
 	// PROD false for production
-	public final static boolean LOGGING = true;
+	public final static boolean LOGGING = false;
 
 	public class PrefNames {
 		public final static String PREFS_FILE = "surespot_preferences";
